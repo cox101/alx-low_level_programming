c60c40361a4195c99c6e0c929598ed7f0fdbab63
cox101 <coxb2659@gmail.com>
2023-09-07 13:05:06 -0700

    initial commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size if a char: %d byte(s)\n", sizeof(char));
+printf("size if an int: %d byte(s)\n", sizeof(int));
+printf("size if a long int: %d byte(s)\n", sizeof(long int));
+printf("size if a long long int: %d byte(s)\n", sizeof(long long int
+));
+printf("size if a float: %d byte(s)\n", sizeof(float));
+}
