be5114d23a3b199c41a6790d3a5c9e453b80f008
cox101 <coxb2659@gmail.com>
2023-09-07 12:34:30 -0700

    initial commmit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-int main(void){
 /**
  * Description of main funtion
  * Return: 0 if success
- */	
+ */
+int main(void){
+
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+
 }
