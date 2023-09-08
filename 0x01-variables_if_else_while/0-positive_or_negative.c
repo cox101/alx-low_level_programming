262a226337a4a89da23e5e225b45d030691d9302
cox101 <coxb2659@gmail.com>
2023-09-08 12:16:26 -0700

    statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,23 @@
-/*
- * File: 0-positive_or_negative.c
- * Auth: cox101
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-/*
-* main - prints a random numbers and states whether
-*         is a positive or a negative or zero
-* Return : 0 If successful
-*/
-int main(void)
-{
-	int n;
+int main() {
+    // Seed the random number generator with the current time
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    // Generate a random number and assign it to the variable 'n'
+    int n = std::rand();
 
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negativee\n", n);
-	else		
-		printf("%d is zero\n", n);
+    // Check if 'n' is positive, zero, or negative and print the result
+    if (n > 0) {
+        std::cout << "The number " << n << " is positive" << std::endl;
+    } else if (n == 0) {
+        std::cout << "The number " << n << " is zero" << std::endl;
+    } else {
+        std::cout << "The number " << n << " is negative" << std::endl;
+    }
 
-	return (0);
+    return 0;
 }
+
