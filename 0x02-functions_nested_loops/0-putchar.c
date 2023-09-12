330bcfa44ca8fe75399c7b35f67d549adb5a8244
cox101 <coxb2659@gmail.com>
2023-09-12 11:10:39 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *alpha = "_putchar";
+
+	while (*alpha)
+	{
+		_putchar(*alpha);
+		alpha++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
