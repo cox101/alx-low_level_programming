3a175d457ef1ac1c1e58993d0095609c063f7772
cox101 <coxb2659@gmail.com>
2023-09-13 12:06:32 -0700

    Initial commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -4,9 +4,9 @@
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
-* month: month in number format
-* day: day of month
-* year: year
+* @month: month in number format
+* @day: day of month
+* @year: year
 * Return: void
 */
 
@@ -34,4 +34,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-}
+}}
