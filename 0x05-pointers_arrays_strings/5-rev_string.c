7bf8d8e82bd23a91dd5cebdb9db090636aa89259
cox101 <coxb2659@gmail.com>
2023-09-17 23:45:16 -0700

    Initial commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * rev_string - reverses a string
  * @s: string to be reversed
@@ -9,11 +10,13 @@ void rev_string(char *s)
 	int i, len, len1;
 	len = 0;
 	len1 = 0;
+
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 	len1 = len - 1;
+
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
