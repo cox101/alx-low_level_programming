d771bf123a768cbd3a0223ee68d05304ace5b0fc
cox101 <coxb2659@gmail.com>
2023-09-25 13:29:23 -0700

    Initial commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,30 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-void set_string(char **s, char *to) {
-    if (s == NULL) {
-        return;  // Return early if the input pointer to char pointer is NULL.
-    }
-
-    // Allocate memory for a new string of the same length as 'to' plus one for the null terminator.
-    *s = (char *)malloc(strlen(to) + 1);
-
-    if (*s != NULL) {
-        strcpy(*s, to);  // Copy the contents of 'to' into the newly allocated memory.
-    }
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s:  The pointer.
+ * @to: The char
+ */
+ 
+void set_string(char **s, char *dest)
+{
+	*s = dest;
 }
-
-int main() {
-    char *str = NULL;  // Declare a char pointer and initialize it to NULL.
-    char newStr[] = "Hello, World!";
-
-    set_string(&str, newStr);  // Call set_string to set the value of 'str' to 'newStr'.
-
-    if (str != NULL) {
-        printf("String: %s\n", str);  // Print the string if it's not NULL.
-        free(str);  // Free the dynamically allocated memory when done.
-    }
-
-    return 0;
-}
-
