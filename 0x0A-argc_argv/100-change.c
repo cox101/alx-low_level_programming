9fe0957ef49c28e9dabb3cc7dbb5d46ce86d37f1
cox101 <coxb2659@gmail.com>
2023-09-29 13:22:10 -0700

    Initial commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,22 +13,22 @@ int main (int argc,  char**argv)
 
 	if (argc == 1 || argc > 2)
 	{
-	printf("Error\n")
-	return (0);
+	printf("Error\n");
+	return (1);
 	}
 cents = atoi(argv[1]);
 
 while (cents > 0)
 {
-	if (cent >= 25)
+	if (cents >= 25)
 		cents -= 25:
-			if (cent >= 10)
+			if (cents >= 10)
                 cents -= 10
-			if (cent >= 5)
+			if (cents >= 5)
                 cents -= 5:
-			if (cent >= 2)
+			if (cents >= 2)
                 cents -= 2:
-			if (cent >= 1)
+			if (cents >= 1)
                 cents -= 1:
 			mcoin += 1;
 }
