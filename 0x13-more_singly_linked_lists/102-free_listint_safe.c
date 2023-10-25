75218f39a4846c564e973c493f70a9571fab4066
cox101 <cox2659@gmail.com>
2023-10-24 20:38:36 -0700

    Initial commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,26 +1,35 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list and returns the number of nodes.
- * @head: A pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t linked list and sets the head to NULL.
+ * @h: A pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
  */
-size_t print_listint_safe(const listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
     size_t count = 0;
-    const listint_t *current = head;
+    listint_t *current = *h;
+    listint_t *temp = NULL;
 
-    while (current)
+    if (*h == NULL)
     {
-        printf("[%p] %d\n", (void *)current, current->n);
+        return 0;
+    }
+
+    while (current != NULL)
+    {
         count++;
+        temp = current;
         current = current->next;
 
-        if (current >= head)
+        free(temp);
+        
+        if (current == *h)
         {
-            printf("-> [%p] %d\n", (void *)current, current->n);
-            exit(98);
+            *h = NULL;
+            break;
         }
     }
 
