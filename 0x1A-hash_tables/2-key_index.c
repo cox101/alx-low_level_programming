edd2b72f162428263d6fcf47cebd1301a35d2b4c
cox101 <cox2659@gmail.com>
2023-12-23 13:02:03 -0800

    Initial commit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,9 +9,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int idx;
-
-	idx = hash_djb2(key);
-
-	return (idx % size);
+	return (hash_djb2(key) % size);
 }
