b72ed2c598efe550d6034d317d12fa27cde08868
cox101 <cox2659@gmail.com>
2023-12-23 13:09:03 -0800

    Initial commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,10 +8,12 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+    hash_table_t *ht;
+
     if (size == 0)
         return NULL;
 
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
+    ht = malloc(sizeof(hash_table_t));
     if (!ht)
         return NULL;
 
