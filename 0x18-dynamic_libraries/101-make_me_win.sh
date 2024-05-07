d1d506189aba3bef1b136112af3721061e6a381b
cox101 <coxb2659@gmail.com>
2024-05-07 14:54:38 -0700

    commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#!/bin/bash
-
 wget -P .. https://raw.githubusercontent.com/cox101/alx-low_level_programming/master/0x18-dynamic_libraries/libjackpot.so
-
 export LD_PRELOAD="$PWD/../libjackpot.so
